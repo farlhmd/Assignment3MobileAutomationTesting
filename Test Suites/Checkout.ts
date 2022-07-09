adf9dfa52a93f05672d2ac76c7aab7896480ad27
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-07-09 13:56:59 +0700

    Create Checkout.ts

---
+++ Test Suites/Checkout.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Checkout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>afaa3054-a00e-4134-844b-2d3f5cf0c585</testSuiteGuid>
+   <testCaseLink>
+      <guid>1e22fe71-fe9e-4e21-959f-8205c61b2968</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Checkout/Checkout - 01</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>17c75d8e-975f-44b0-bd59-98a5acd2ab67</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Excel Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>643edf34-6733-448f-81a3-7803dc8302cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Checkout/Checkout - 02</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>c4657ea3-3a19-4362-a039-bddcc08c36ae</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Excel Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
